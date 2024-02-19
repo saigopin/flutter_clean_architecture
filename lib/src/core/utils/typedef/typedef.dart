1a80271963f517946c6294176c89cf55df00362b
arorajasman <arorajasman97@gmail.com>
2024-02-19 11:00:48 +0530

    added changes

--- lib/src/core/utils/typedef/typedef.dart
+++ lib/src/core/utils/typedef/typedef.dart
@@ -4,3 +4,5 @@ import 'package:flutter_clean_architecture/src/core/errors/failures.dart';
 typedef DataMap = Map<String, dynamic>;
 
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
+
+typedef ResultVoid = ResultFuture<void>;
