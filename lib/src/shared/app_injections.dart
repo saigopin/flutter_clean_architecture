0aeb523927f69ad6464f9d830b2d3685b657f392
“GonaJanardhanReddy” <gonar@byteridge.com>
2024-02-27 13:40:36 +0530

    added dependency injection to themeBloc

--- lib/src/shared/app_injections.dart
+++ lib/src/shared/app_injections.dart
@@ -1,6 +1,10 @@
+import 'package:flutter_clean_architecture/src/core/exports.dart';
 import 'package:flutter_clean_architecture/src/core/utils/injections.dart';
 import 'package:flutter_clean_architecture/src/shared/data/local_data/app_shared_prefs.dart';
 
 void initAppInjections() {
   sl.registerFactory<AppSharedPrefs>(() => AppSharedPrefs(sl()));
+
+  // Register ThemeSwitchBloc as a singleton
+  sl.registerFactory<ThemeSwitchBloc>(() => ThemeSwitchBloc());
 }
