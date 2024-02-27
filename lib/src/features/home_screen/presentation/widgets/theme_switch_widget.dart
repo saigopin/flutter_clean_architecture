6bf20bd4d046b5ac4e90d258a11fb7ba0928a8f4
“ReddyGona” <reddy.gona1122@gmail.com>
2024-02-27 11:20:58 +0530

    updated theme with provider

--- lib/src/features/home_screen/presentation/widgets/theme_switch_widget.dart
+++ lib/src/features/home_screen/presentation/widgets/theme_switch_widget.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_clean_architecture/src/features/home_screen/presentation/bloc/theme_switch/theme_switch_bloc.dart';
+import 'package:flutter_clean_architecture/src/core/exports.dart';
+import 'package:provider/provider.dart';
 
 class ThemeSwitchWidget extends StatelessWidget {
   const ThemeSwitchWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeSwitchBloc, ThemeSwitchState>(
-        builder: (BuildContext context, ThemeSwitchState state) {
-      return IconButton(
-        onPressed: () {
-          if (state.switchValue) {
-            context.read<ThemeSwitchBloc>().add(SwitchOffEvent());
-          } else {
-            context.read<ThemeSwitchBloc>().add(SwitchOnEvent());
-          }
-        },
-        icon: Icon(
-          state.switchValue ? Icons.sunny : Icons.dark_mode_outlined,
-        ),
-      );
-    });
+    // Obtain the ThemeNotifier instance using Provider
+    ThemeNotifier themeNotifier = Provider.of<ThemeNotifier>(context);
+
+    return IconButton(
+      onPressed: () {
+        // Toggle the theme using the ThemeNotifier
+        themeNotifier.toggleTheme();
+      },
+      icon: Icon(
+        themeNotifier.darkTheme ? Icons.sunny : Icons.dark_mode_outlined,
+      ),
+    );
   }
 }
