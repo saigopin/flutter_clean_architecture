acf6a9adef59cb65971fb6d50e12c7d22effb7d2
“GonaJanardhanReddy” <gonar@byteridge.com>
2024-02-28 14:14:29 +0530

    corrected imports

--- lib/src/core/utils/injections.dart
+++ lib/src/core/utils/injections.dart
@@ -3,7 +3,7 @@ import 'package:flutter_clean_architecture/src/core/network/dio/dio_operations.d
 import 'package:flutter_clean_architecture/src/features/home/articles_injections.dart';
 import 'package:flutter_clean_architecture/src/shared/app_injections.dart';
 import 'package:get_it/get_it.dart';
-import 'package:secure_shared_preferences/secure_shared_preferences.dart';
+import 'package:secure_shared_preferences/secure_shared_pref.dart';
 import 'loggers/app_logger.dart';
 
 final GetIt getIt = GetIt.instance;
